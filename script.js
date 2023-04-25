c36aa7f46810f7ad214b5171bffcdd922f409964
akshatpare <76091015+akshatpare@users.noreply.github.com>
2023-04-25 19:58:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ setInterval(() =>{
    const icon = document.querySelector(".section-1-icons .change")
    
    icon.classList.remove("change")
+   
+   if (i > icons.length){
+      
+   }   
+   
    icon.nextElementSibling.classList.add("change")
 
 }, 1000)
